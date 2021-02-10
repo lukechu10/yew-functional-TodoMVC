7742cfd816a567b02330b0d0c35383fb31dff9a9
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2021-02-10 15:16:06 -0800

    Migrate to trunk

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,7 @@ use yew::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // This is the entry point for the web app
-#[wasm_bindgen]
-pub fn run_app() -> Result<(), JsValue> {
+pub fn main() -> Result<(), JsValue> {
     wasm_logger::init(wasm_logger::Config::default());
     yew::start_app::<app::App>();
     Ok(())
